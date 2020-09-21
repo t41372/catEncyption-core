adaa3ddd7b4c5ddc07167e87d0d0ee0ba69d981c
Tim Chiu <mytim710@163.com>
2020-09-21 13:12:15 +0800

    initial update

---
+++ catEncyption-core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace catEncyption_core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
